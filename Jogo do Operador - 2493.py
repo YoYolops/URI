45ebe323bb11ca5252ef336b22dd331d4b0eb3d3
YoYolops <yohanhxh@gmail.com>
2020-06-28 04:50:05 -0300

    eles passarão, eu passarinho

--- Jogo do Operador - 2493.py
+++ Jogo do Operador - 2493.py
@@ -1,6 +1,20 @@
+# Runtime Error dentro do while
 
-
-
+a = int(input())
+n = 1
+for i in range(a):
+    pa, pb, g1, g2 = input().split()
+    pa = int(pa)
+    pb = int(pb)
+    g1 = float(g1)
+    g2 = float(g2)
+    popa = pa + int(pa * g1)
+    popb = pb + int(pb * g2)
+    while popa != popb:
+        popa += int(popa * g1)
+        popb += int(popb * g2)
+        n += n
+        print("%d anos"%n)
 
 
 # JUST CODING (FROM BRASIL!)

645c9b9421f08ff82e9c73f53a068da6cf6f26a5
YoYolops <yohanhxh@gmail.com>
2024-04-30 14:23:44 -0300

    Implementa solucao problema ad-hoc 1732

--- ad-hoc/abelha_maja_1732.rs
+++ ad-hoc/abelha_maja_1732.rs
@@ -3,25 +3,25 @@
 use std::io;
 use std::collections::HashMap;
 
-fn read_integer() -> i32 {
+fn read_integer() -> isize {
     let mut input = String::new();
     io::stdin().read_line(&mut input)
         .expect("Failed to read line");
 
-    let parsed_input: i32 = input.trim().parse()
+    let parsed_input: isize = input.trim().parse()
         .expect("Failed to parse integer");
 
     parsed_input
 }
 
-fn get_max_level_value(level: &i32) -> i32 {
+fn get_max_level_value(level: &isize) -> isize {
     (3*level.pow(2)) - (3*level) + 1
 }
 
-fn solver() -> (i32, i32) {
+fn solver() -> (isize, isize) {
     let target = read_integer();
     let mut level = 1;
-    let mut max_level_value: i32;
+    let mut max_level_value: isize;
     loop {
         max_level_value = get_max_level_value(&level);
 
@@ -34,8 +34,8 @@ fn solver() -> (i32, i32) {
         }
         level += 1;
     }
-    /* println!("level {}", level); */
-    let mut map: HashMap<String, i32> = HashMap::new();
+
+    let mut map: HashMap<String, isize> = HashMap::new();
     map.insert(String::from("up"), level-1); /* maximo de pulos que podem ser feitos nessa direção */
     map.insert(String::from("left"), (level-1)*2);
     map.insert(String::from("down"), level-1);
@@ -52,28 +52,30 @@ fn solver() -> (i32, i32) {
     let mut current_movement_type_index = 0;
     for _ in 0..jumps_needed { /* Executa os saltos até chegar ao target */
         if let Some(value) = map.get_mut(&movements[current_movement_type_index]) {
-            *value -= 1;
             match movements[current_movement_type_index].as_str() {
                 "up" => {
                     current_binary_coordinates_address = (current_binary_coordinates_address.0, current_binary_coordinates_address.1 - 1);
-                    println!("MOVING UP");
                 }
                 "left" => {
-                    current_binary_coordinates_address = (current_binary_coordinates_address.0 - 1, current_binary_coordinates_address.1);
-                    println!("MOVING LEFT");
+                    current_binary_coordinates_address = (
+                        current_binary_coordinates_address.0 - 1,
+                        if *value <= (level-1) { current_binary_coordinates_address.1 + 1 } else { current_binary_coordinates_address.1 }
+                    )
                 }
                 "right" => {
-                    current_binary_coordinates_address = (current_binary_coordinates_address.0 + 1, current_binary_coordinates_address.1);
-                    println!("MOVING RIGHT");
+                    current_binary_coordinates_address = (
+                        current_binary_coordinates_address.0 + 1,
+                        if *value <= (level-1) {current_binary_coordinates_address.1 - 1} else { current_binary_coordinates_address.1 }
+                    );
                 }
                 "down" => {
                     current_binary_coordinates_address = (current_binary_coordinates_address.0, current_binary_coordinates_address.1 + 1);
-                    println!("MOVING DOWN");
                 }
                 _ => {
                     println!("No patterns matched");
                 }
             }
+            *value -= 1;
             if *value == 0 {
                 current_movement_type_index += 1;
             }

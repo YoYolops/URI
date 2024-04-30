548589eb46898d3300c6915497a18507036ba2b8
YoYolops <yohanhxh@gmail.com>
2024-04-29 22:16:36 -0300

    Implementa logica parcial de atualizacao de par ordenado ao andar casas

--- ad-hoc/abelha_maja_1732.rs
+++ ad-hoc/abelha_maja_1732.rs
@@ -34,31 +34,57 @@ fn solver() -> (i32, i32) {
         }
         level += 1;
     }
-
+    /* println!("level {}", level); */
     let mut map: HashMap<String, i32> = HashMap::new();
-    map.insert(String::from("up"), level); /* maximo de pulos que podem ser feitos nessa direção */
-    map.insert(String::from("left"), level*2);
-    map.insert(String::from("down"), level);
-    map.insert(String::from("right"), level*2);
+    map.insert(String::from("up"), level-1); /* maximo de pulos que podem ser feitos nessa direção */
+    map.insert(String::from("left"), (level-1)*2);
+    map.insert(String::from("down"), level-1);
+    map.insert(String::from("right"), (level-1)*2);
 
+    let movements: [String; 4] = [
+        String::from("up"),
+        String::from("left"),
+        String::from("down"),
+        String::from("right")
+    ];
     let mut current_binary_coordinates_address = (level-1, 0);
     let jumps_needed = max_level_value - target;
-    let mut current_square_value = max_level_value;
-
+    let mut current_movement_type_index = 0;
     for _ in 0..jumps_needed { /* Executa os saltos até chegar ao target */
-        const movements = ["up", "left", "down", "right"];
+        if let Some(value) = map.get_mut(&movements[current_movement_type_index]) {
+            *value -= 1;
+            match movements[current_movement_type_index].as_str() {
+                "up" => {
+                    current_binary_coordinates_address = (current_binary_coordinates_address.0, current_binary_coordinates_address.1 - 1);
+                    println!("MOVING UP");
+                }
+                "left" => {
+                    current_binary_coordinates_address = (current_binary_coordinates_address.0 - 1, current_binary_coordinates_address.1);
+                    println!("MOVING LEFT");
+                }
+                "right" => {
+                    current_binary_coordinates_address = (current_binary_coordinates_address.0 + 1, current_binary_coordinates_address.1);
+                    println!("MOVING RIGHT");
+                }
+                "down" => {
+                    current_binary_coordinates_address = (current_binary_coordinates_address.0, current_binary_coordinates_address.1 + 1);
+                    println!("MOVING DOWN");
+                }
+                _ => {
+                    println!("No patterns matched");
+                }
+            }
+            if *value == 0 {
+                current_movement_type_index += 1;
+            }
+        }
     }
-
-    /* to go left => x-1 */
-    /*  to go right => x+1 */
-    /*  to go up y-1 */
-    /* to go down y+1 */
-    return (10, 10)
+    current_binary_coordinates_address
 }
 
 fn main() {
     loop {
         let res = solver();
-        println!("{}", res.0);
+        println!("{} {}", res.0, res.1);
     }
 }
